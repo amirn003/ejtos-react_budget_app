71c06228e6efea0dba6cd6beaef155d2f02f5a21
amirn003 <amir.naar@gmail.com>
2023-02-27 18:46:19 -0500

    budget allocation validation, editable budget value

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+    const { expenses } = useContext(AppContext);
+    const [editableBudget, setEditableBudget] = useState(budget);
+
+    const handleBudgetChange = (e) => {
+        const newBudget = e.target.value;
+        setEditableBudget(newBudget);
+        dispatch({ type: 'SET_BUDGET', payload: newBudget });
+        if (newBudget > 20000){
+            alert("Warning! Your budget exceed 20000!")
+            return;
+        }
+        if (newBudget < expenses){
+            alert("Warning! Your budget is lower than the expenses ",expenses,"!")
+            return;
+        }
+
+    }
+    
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £  
+                <input
+                    required='required'
+                    type='number'
+                    id='budget'
+                    value={editableBudget}
+                    onChange={handleBudgetChange}
+                />
+            </span>
         </div>
     );
+    
 };
+
 export default Budget;
